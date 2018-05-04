2b250485de4fc55fe717c8dfd1552aa39c83b984
Phase Matrix <pmatrixmedia@gmail.com>
2018-05-03 22:58:27 -0500

    version 2.0

--- functions/index.js
+++ functions/index.js
@@ -14,10 +14,34 @@ admin.initializeApp(functions.config().firebase);
 
 
 app.get(
+    ["/","/:id"],
+    functions.https.onRequest((req, res) => {
+        const friendsid = req.params.id;
+        console.log(friendsid)
+        let reference = "friends";
+        reference += friendsid ? "/" + friendsid : "";
+
+        cors(req, res, () => {
+            return admin
+                .database()
+                .ref()
+                .once("value")
+                .then((snapshot) => {
+                    if (snapshot.val() !== null) {
+                        res.status(200).send(JSON.stringify(snapshot));
+                    } else {
+                        res.status(200).send({});
+                    }
+                });
+        });
+    })
+);
+
+app.post(
     ["/", "/:id"],
     functions.https.onRequest((req, res) => {
         const friendsid = req.params.id;
-        let reference = "friends";
+        let reference = "api/friends";
         reference += friendsid ? "/" + friendsid : "";
 
         cors(req, res, () => {
@@ -42,10 +66,11 @@ app.use(bodyParser.json());
 
 // loads static files
 app.use(express.static("../app/public"));
+app.use(express.static("../app/routing"));
 
 
 
-// List all the posts under the path /posts/
+// List all the posts under the path /friends/
 exports.friends = functions.https.onRequest((req, res) => {
     // Handle routing of /posts without a trailing /,
     if (!req.path) {
